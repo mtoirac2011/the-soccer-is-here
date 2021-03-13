2a801f88154fff439cf181232c7d2f4a1a57b130
Minardo <mtoirac2011@gmail.com>
2021-03-13 13:07:09 -0500

    history-video and leagues

---
+++ js/history.js
@@ -0,0 +1,18 @@
+
+var miVideo=document.getElementById("video1");
+
+function playPause(){
+    if (miVideo.paused)
+       miVideo.play();
+    else
+        miVideo.pause();
+}
+
+function stop(){
+    miVideo.pause();
+    miVideo.currentTime=0;
+}
+
+function skip(value){
+    miVideo.currentTime += value;
+}
